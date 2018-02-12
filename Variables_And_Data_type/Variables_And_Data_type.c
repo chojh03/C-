a5d9bbfd407ddcc5674ff992356ad1bcdd4dd2e5
9host1st <9host1st@gmail.com>
2018-02-13 06:48:41 +0900

    Variable_and_Data_type

---
+++ Variables_And_Data_type/Variables_And_Data_type.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 1;
+  char b = "a";
+  float c = 1.123;
+  double d = 1.2334;
+  long e = 11212;
+
+  printf("int : %d\n", a);
+  printf("char : %s\n", b)
+  printf("float : %f\n", c);
+  printf("double : %lf\n", d);
+  printf("long : %ld\n", f);
+}
